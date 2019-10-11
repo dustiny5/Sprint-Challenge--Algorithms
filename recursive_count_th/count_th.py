333bd0ef2e88889cd0addfb5e911948a6cef48d1
dustiny5 <dustiny5@users.noreply.github.com>
2019-10-11 11:17:11 -0700

    Delete count variable

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,7 +3,6 @@ Your function should take in a signle parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-count = 0
 
 def count_th(word):
   count = 0
